7837db56761a159d2cd79c397f4bccb573f67b2a
joya <joyajabbour79@gmail.com>
2026-04-17 22:15:30 +0300

    fix folders

--- BusBookingSystem/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BusBookingSystem.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
